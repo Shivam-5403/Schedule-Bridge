cc93ac2cdec331b9acf17c6b00f356a846939b44
Bhavyajprajapati <bhavyaprajapati245@gmail.com>
2024-08-24 18:49:24 +0530

    bha 7th

--- Routes/Route.js
+++ Routes/Route.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Login, About, Contact, Members } = require('../controller/LoginP');
 
 router.get('/login', Login);
+router.post('/login', Login);
 router.get('/About', About);
 router.get('/Contact', Contact);
 router.get('/Members', Members);
