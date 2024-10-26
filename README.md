5969f7bd2ee70759a120b4b50403799a6043a6d8
Bhavyajprajapati <157686673+Bhavyajprajapati@users.noreply.github.com>
2024-10-26 14:55:54 +0530

    JWT
    added

---
+++ README.md
@@ -0,0 +1,4 @@
+# For JWT(JSON Web Token)
+-> if not Work then Install :- npm i jsonwebtoken
+
+# For env file
--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { loginRequired } = require('../Middleware/session');
-const { Home, About, Contact, Members, User_Profile, Login, G_signup, forgot_password, G_verification, view_profile, BookApp, ViewApp, login, P_signup, P_changepassword, P_verification, change_pass, change_prof } = require('../Controller/UserController')
+const path = require('path');
+const Authenticate = require('../Middleware/Authenticate');
+const {
+    Home, About, Contact, Members, User_Profile, Login, G_signup,
+    forgot_password, G_verification, view_profile, BookApp, ViewApp,
+    login, P_signup, P_changepassword, P_verification,
+    change_pass, change_prof
+} = require('../Controller/UserController');
 
 router.get('/', Login);
 router.get('/signup', G_signup);
@@ -10,14 +16,15 @@ router.get('/verification', G_verification);
 router.post('/Home', login);
 router.post('/signup', P_signup);
 router.post('/verification', P_verification);
+router.post('/changepassword', P_changepassword);
+
+router.use(Authenticate);
 
 router.get('/User-Profile', User_Profile);
 router.get('/view-profile', view_profile);
-router.post('/changepassword', P_changepassword);
 router.post('/change-pass', change_pass);
 router.post('/change-prof', change_prof);
-
-router.get('/Home', loginRequired, Home);
+router.get('/Home', Home);
 router.get('/About', About);
 router.get('/Contact', Contact);
 router.get('/Members', Members);
